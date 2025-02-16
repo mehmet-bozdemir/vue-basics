d10cb627bbe9c40d6d2afbf68bc24866bfb2bbed
Adam Timmerman <m.b.@macmini.home>
2025-02-16 21:26:33 +0100

    applied composables

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,13 @@
+<script setup>
+import {useFlash} from "@/composables/useFlash";
+let {flash} = useFlash();
+
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="flash('yay!','it works!', 'success')">click me</button>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,32 @@
 <script setup>
-import { ref } from 'vue';
 import TheWelcome from '../components/TheWelcome.vue'
+import TabbableTextarea from '@/components/TabbableTextarea.vue'
+import { useFlash } from '@/composables/useFlash'
+import { useStorage } from '@/composables/useStorage'
+let { flash } = useFlash()
+import {ref} from 'vue'
 
-let message = ref('hello world');
+let comment = ref('initial textarea value');
+let food = useStorage('food', 'tacos')
+let age = useStorage('age', 29)
 
+let obj = useStorage('obj', { one: 'one' })
+
+// setTimeout(()=>{
+//   obj.value = {'changed': 'entirely'}
+// },3000)
 </script>
 
 <template>
   <main>
     <!-- <TheWelcome /> -->
-    
-    {{message}}
+    <form>
+      <TabbableTextarea v-model="comment" style="width: 100%; height: 300px" />
+    </form>
+    <p>what is your favorite food? <input type="text" v-model="food" /></p>
+
+    <p>how old are you? <input type="text" v-model="age" /></p>
+
+    <button @click="flash('yay!', 'home', 'info')">click me</button>
   </main>
 </template>
