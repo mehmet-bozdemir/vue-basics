c2e73caee7b938e9ba88fab1fa50ddbad63d58f8
Adam Timmerman <m.b.@macmini.home>
2025-02-16 11:25:24 +0100

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+import TheWelcome from '../components/TheWelcome.vue'
+
+let message = ref('hello world');
+
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    
+    {{message}}
+  </main>
+</template>
